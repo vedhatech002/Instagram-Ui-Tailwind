3e81b9b3072d979dae0c029954035d4427d83659
Vijayavedhasekarn <vedhatech02@gmail.com>
2023-10-04 20:03:15 +0530

    adding responsive for all mobile devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,10 @@ module.exports = {
         // => @media (min-width: 908px) { ... }
         'lsm': '684px',
         // => @media (min-width: 684px) { ... }
+        'lmb': '500px',
+        // => @media (min-width: 500px) { ... }
+        'mb': '320px',
+        // => @media (min-width: 500px) { ... }
 
       },
     },
